be94fa1c6ae3c5ef953fa5b5363460ae19d93873
naozumi23 <naozumi1024@outlook.jp>
2022-10-05 00:13:48 +0900

    git first commit

---
+++ OneDrive/デスクトップ/Playing Pytyon/DjangoTest/DjangoTest/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\naozu\OneDrive\デスクトップ\Playing Pytyon\DjangoTest\DjangoTest\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
