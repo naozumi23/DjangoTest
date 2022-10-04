e1e6674fd9698c32053d798f33d4063bf5a3cc2b
naozumi23 <naozumi1024@outlook.jp>
2022-10-05 00:50:47 +0900

    GitTestの追加

---
+++ OneDrive/デスクトップ/Playing C#/GitTest/GitTest/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitTest.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TestView1()
+        {
+            return View();
+        }
+    }
+}
